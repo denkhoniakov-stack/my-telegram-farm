358c10c0b0bb0333c1851190ccb7d43f72c6f417
denkhoniakov-stack <den.khoniakov@mail.ru>
2025-11-23 14:20:45 +0200

    Add files via upload

--- lab/hybrids.js
+++ lab/hybrids.js
@@ -548,28 +548,9 @@ function initHybridLab(gameState, tg, updateBalanceDisplay, saveGameData, PLANT_
             if (gameState.warehouse[crop1] <= 0) delete gameState.warehouse[crop1];
             if (gameState.warehouse[crop2] <= 0) delete gameState.warehouse[crop2];
             
-            
-            // --- БОНУС: Ускорение лаборатории (lab) ---
-            let duration = stats.growTime;
-            let labSpeedBonus = 0;
-
-            if (gameState.farmers) {
-                gameState.farmers.forEach(farmer => {
-                    if (farmer.bonusType === 'lab') {
-                        labSpeedBonus += farmer.bonusValue;
-                    }
-                });
-            }
-
-            if (labSpeedBonus > 0) {
-                // Уменьшаем время на процент бонуса
-                duration = duration * (1 - labSpeedBonus / 100);
-            }
-            // ------------------------------------------
-
             gameState.hybridMixings[activeRarity] = { 
                 startTime: Date.now(), 
-                duration: duration, 
+                duration: stats.growTime, 
                 resultEmoji: stats.resultEmoji, 
                 resultName: stats.name, 
                 crop1: crop1, 
